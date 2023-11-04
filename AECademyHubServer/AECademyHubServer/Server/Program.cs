31e59079d747967f3c464f97d9e2a189c510f9d1
Chenzhang Wang <51490717+czwangxtt@users.noreply.github.com>
2023-11-04 15:16:01 -0400

    Add: swaggle UI for API test

--- AECademyHubServer/AECademyHubServer/Server/Program.cs
+++ AECademyHubServer/AECademyHubServer/Server/Program.cs
@@ -6,8 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +24,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
